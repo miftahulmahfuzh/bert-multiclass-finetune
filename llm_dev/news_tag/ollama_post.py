e4842316f076f2a8c38a820cda8f4860e8a38be0
miftahulmahfuzh <mahfuzh74@gmail.com>
2025-02-06 16:10:18 +0700

    added xlmr large result

--- llm_dev/news_tag/ollama_post.py
+++ llm_dev/news_tag/ollama_post.py
@@ -11,7 +11,7 @@ def call_ollama_api(prompt):
     url = "http://localhost:11435/api/generate"
 
     payload = {
-        "model": "deepseek-r1:14b",
+        "model": "deepseek-r1:32b",
         "prompt": prompt,
         "temperature": 0,
         "top_p": 1,      # Set to 1 to disable top-p sampling
@@ -34,11 +34,14 @@ def write_answer_file(answer, filename):
 
 def main():
     prompt = read_prompt_file('prompt_v2.txt')
-    fnews = "/home/devmiftahul/nlp/news_tag/news/news_4831022.txt"
-    news_str = read_prompt_file(fnews)
+    # fnews = "/home/devmiftahul/nlp/news_tag/news/news_4831022.txt" # a1
+    # fnews = "/home/devmiftahul/nlp/news_tag/news/news_4837053.txt" # a2
+    # fnews = "/home/devmiftahul/nlp/news_tag/news/news_4831071.txt" # a3
+    # news_str = read_prompt_file(fnews)
+    news_str = "$TBLA - Taboola Signs memasuki kesepakatan eksklusif baru selama lima tahun dengan Grey Television"
     prompt_content = prompt.replace("<<NEWS>>", news_str)
     print(prompt_content)
-    print("====================================")
+    print("============================================")
 
     # Get LLM response
     llm_response = call_ollama_api(prompt_content)
