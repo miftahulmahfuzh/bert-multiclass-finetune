1fbed3c2c6f9db028455597366c369f978d0bfd0
miftahulmahfuzh <mahfuzh74@gmail.com>
2025-03-13 11:34:08 +0700

    added prev_questions info in prompt

--- tuntun_chatbot.py
+++ tuntun_chatbot.py
@@ -1,7 +1,8 @@
 # %% IMPORTS
+import time
 from datetime import datetime
 
-import gradio as grq
+import gradio as gr
 from langchain.chains.retrieval_qa.base import RetrievalQA
 
 from core.model import llm_ollama
@@ -13,9 +14,12 @@ from tool.tool_caller import process_tools
 def combine_docs(docs):
     return "\n\n".join(doc.metadata['Answer'] for doc in docs)
 
+prev_questions = []
 
 def rag_chain(question, history):
-    processed_prompt = prompt.partial(tools_output=process_tools(question))
+    global prev_questions
+    prev_questions_str = "\n".join(prev_questions)
+    processed_prompt = prompt.partial(tools_output=process_tools(question), prev_questions=prev_questions_str)
     qa = RetrievalQA.from_chain_type(llm=llm_ollama,
                                      chain_type="stuff",
                                      chain_type_kwargs={"prompt": processed_prompt,
@@ -25,17 +29,19 @@ def rag_chain(question, history):
                                      retriever=vectorstore_none.as_retriever(),
                                      verbose=True)
     print("Timestamp: " + str(datetime.today()))
-    return qa.invoke(question,
-                     return_only_outputs='result').get(
-        "result")
+    # return qa.invoke(question,
+    #                  return_only_outputs='result').get(
+    #     "result")
     # return json.loads(faq_selector(question))["data"]
 
-    #     # partial_message = ""
-    #     # for response in qa.invoke(question, return_only_outputs='result').get("result"):
-    #     #     # for response in res:
-    #     #     partial_message += response
-    #     #     # time.sleep(0.001)
-    #     #     yield partial_message
+    partial_message = ""
+    for response in qa.invoke(question, return_only_outputs='result').get("result"):
+        # for response in res:
+        partial_message += response
+        time.sleep(0.005)
+        yield partial_message
+
+    prev_questions.append(question)
     #
     #
 
